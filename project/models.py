5af6ead4b5b1ac87a5338a7b35f57b2f50efa5d3
GiuseppeDiMaio99 <47873236+GiuseppeDiMaio99@users.noreply.github.com>
2021-02-07 22:04:07 -0500

    Update models.py

--- project/models.py
+++ project/models.py
@@ -8,19 +8,19 @@ class User(UserMixin, db.Model):
 	name = db.Column(db.String(1000))
 	needs_reset = db.Column(db.Boolean)
 	
-class Employee(UserMixin, db.model):
+class Employee(db.Model):
 	employeeID = db.Column(db.Integer, primary_key=True, unique=True)
 	firstName = db.Column(db.String(100))
 	lastName = db.Coulmn(db.String(100))
 	jobTitle = db.Coulmn(db.String(100))
 	payRate = db.Coulmn(db.Double)
 	
-class Project(UserMixin, db.model):
+class Project(db.Model):
 	projectID = db.Coulmn(db.Integer, primary_key=True, unique=True)
 	projectName = db.Coulmn(db.String(100))
 	projectOngoing = db.Coumn(db.Boolean)
 	
-class TimeLog(UserMixin, db.model):
+class TimeLog(db.Model):
 	projectID = db.Coulmn(db.Integer, primary_key=True)
 	employeeID = db.Coulmn(db.Integer)
 	currentTime = db.Coulmn(db.date)
