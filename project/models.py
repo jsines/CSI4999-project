daeca433f7cedda246f6c8968ea44437c95c5987
GeorgeFeliciano98 <77352489+GeorgeFeliciano98@users.noreply.github.com>
2021-02-28 22:14:14 -0500

    Update models.py

--- project/models.py
+++ project/models.py
@@ -25,11 +25,20 @@ class Project(db.Model):
 	projectID = db.Column(db.Integer, primary_key=True, unique=True)
 	projectName = db.Column(db.String(100))
 	projectOngoing = db.Column(db.Boolean)
+	EmployerID = db.Column(db.Integer)
 	
 #this creates TimeLog table 
 class TimeLog(db.Model):
-	projectID = db.Column(db.Integer, primary_key=True)
-	employeeID = db.Column(db.Integer)
+	TimeLogID = db.Column(db.Integer, primary_key=True)
+	projectName = db.Column(db.String(100))
 	currentTime = db.Column(db.Date)
 	time = db.Column(db.Integer)
+
+#this creates the Assignment table (where all project-employee asignments go and can be read)
+class Assignments(db.Model):
+	AssignmentID = db.Column(db.Integer, primary_key=True) #this is a table identifier; not actually used
+	projectName = db.Column(db.String(100))                #this is the name for the selected project
+	employeeID = db.Column(db.Integer)                     #this will be entered by the employer; this is the "assigned" employee
+	UserID = db.Column(db.Integer)                         #this will be used for visibility checks
+
 	
