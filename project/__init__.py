235a71108aa04eb4739868c7e3db9c9f9d32fc42
GiuseppeDiMaio99 <47873236+GiuseppeDiMaio99@users.noreply.github.com>
2021-04-18 12:22:49 -0400

    Add files via upload

--- project/__init__.py
+++ project/__init__.py
@@ -23,7 +23,7 @@ def create_app():
 
 	app.config['MAIL_SERVER']='smtp.gmail.com'
 	app.config['MAIL_PORT'] = 465
-	app.config['MAIL_USERNAME'] = 'dcaatimemamangement@gmail.com'
+	app.config['MAIL_USERNAME'] = 'dcaatimemanagement@gmail.com'
 	app.config['MAIL_PASSWORD'] = os.environ.get('MAILPWD')
 	app.config['MAIL_USE_TLS'] = False
 	app.config['MAIL_USE_SSL'] = True
